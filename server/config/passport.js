4074dbef9b918f40c2e6ecdf1712654fe0072c65
Smart Coder <rafiaperween09@gmail.com>
2025-09-11 01:45:49 +0530

    oauth

--- server/config/passport.js
+++ server/config/passport.js
@@ -27,7 +27,7 @@ async function findOrCreateUser(profile,provider){
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://techtalesapp.netlify.app/oauth/google/callback"
+    callbackURL: "https://techtales-1-ez4f.onrender.com/oauth/google/callback"
 },
 async function(accessToken, refreshToken, profile, cb) {
     const user=await findOrCreateUser(profile,"google");
