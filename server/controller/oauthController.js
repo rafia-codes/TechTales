edcbc298f062c6fac551f858d1d7fbedb7370b32
Smart Coder <rafiaperween09@gmail.com>
2025-09-12 00:54:39 +0530

    oauth

--- server/controller/oauthController.js
+++ server/controller/oauthController.js
@@ -1,10 +1,10 @@
-const {generateTokens,setAuthCookies}=require('../utils/tokenmanager');
+const { generateTokens, setAuthCookies } = require("../utils/tokenmanager");
 
-module.exports.oauthsuccess=async(req,res)=>{
-    if(!req.user)
-        res.status(401).json({message:"Authentication failed"});
-    const {accesstoken,refreshtoken}=generateTokens(req.user);
-    setAuthCookies(res,accesstoken,refreshtoken);
-    res.redirect('https://techtalesapp.netlify.app/discover');
+module.exports.oauthsuccess = async (req, res) => {
+  if (!req.user) res.status(401).json({ message: "Authentication failed" });
+  const { accesstoken, refreshtoken } = generateTokens(req.user);
+  setAuthCookies(res, accesstoken, refreshtoken);
+  console.log("Cookies:", req.cookies.accesstoken);
+  console.log("AccessToken:", req.cookies.refreshtoken);
+  res.redirect("https://techtalesapp.netlify.app/discover");
 };
-
